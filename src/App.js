7bd2434867eb30745b4022bbf3a3f3fbcde8971d
julius ndakala <juliusndakala35@gmail.com>
2023-05-30 13:17:38 +0300

    updated the App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Projects from "./Pages/Projects";
+import Technologies from "./Pages/Technologies";
 function App() {
-  return <div className="App">
-  
-  
-  </div>;
+  return (
+    <div className="dark">
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/technologies" element={<Technologies />} />
+          </Routes>
+          <Footer />
+        </Router>
+    </div>
+  );
 }
-
 export default App;
